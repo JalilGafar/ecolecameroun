603069b0880383cd8b5b3acf715f40ca0371c76c
Fonda Njiemessa Jalil Gafar <fondajalil@hotmail.com>
2022-12-15 05:28:13 +0100

    creation etude-info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'etude-info', loadChildren: () => import('./etude-info/etude-info.module').then(m => m.EtudeInfoModule) },
+  { path: '**', redirectTo: 'etude-info'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/etude-info/etude-info.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EtudeInfoRoutingModule } from './etude-info-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EtudeInfoRoutingModule
+  ]
+})
+export class EtudeInfoModule { }
